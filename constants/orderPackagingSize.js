42b59ca0d40fb524d587746ae9098f0556ed959b
gbagul <gbagul@avaya.com>
2022-07-08 16:57:37 +0530

    Added constants and prettier

---
+++ constants/orderPackagingSize.js
@@ -0,0 +1,9 @@
+const SMALL_ORDER = 3
+const MEDIUM_ORDER = 7
+const LARGE_ORDER = 10
+
+module.exports = {
+  SMALL_ORDER,
+  MEDIUM_ORDER,
+  LARGE_ORDER
+}
---
+++ constants/orderType.js
@@ -0,0 +1,7 @@
+const TAKEAWAY = 'Take away'
+const DINE_IN = 'Dine In'
+
+module.exports = {
+  TAKEAWAY,
+  DINE_IN
+}
